c2a6cd1fcd8414ee8e44dfdc57548ff9eea6816d
NSTiwari <tiwarinitin1999@gmail.com>
2021-01-25 00:27:21 +0530

    Cartoon Classification using TF Lite

---
+++ app/src/main/java/com/example/cartoonclassification/MainActivity.java
@@ -0,0 +1,270 @@
+package com.example.cartoonclassification;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.content.res.AssetFileDescriptor;
+import android.graphics.Bitmap;
+import android.graphics.Color;
+import android.net.Uri;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.SeekBar;
+import android.widget.TextView;
+
+import com.example.cartoonclassification.R;
+
+import org.tensorflow.lite.DataType;
+import org.tensorflow.lite.Interpreter;
+import org.tensorflow.lite.support.common.FileUtil;
+import org.tensorflow.lite.support.common.TensorOperator;
+import org.tensorflow.lite.support.common.TensorProcessor;
+import org.tensorflow.lite.support.common.ops.NormalizeOp;
+import org.tensorflow.lite.support.image.ImageProcessor;
+import org.tensorflow.lite.support.image.TensorImage;
+import org.tensorflow.lite.support.image.ops.ResizeOp;
+import org.tensorflow.lite.support.image.ops.ResizeWithCropOrPadOp;
+import org.tensorflow.lite.support.label.TensorLabel;
+import org.tensorflow.lite.support.tensorbuffer.TensorBuffer;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.nio.MappedByteBuffer;
+import java.nio.channels.FileChannel;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+
+
+import com.github.mikephil.charting.charts.BarChart;
+import com.github.mikephil.charting.charts.HorizontalBarChart;
+import com.github.mikephil.charting.components.Legend;
+import com.github.mikephil.charting.components.XAxis;
+import com.github.mikephil.charting.data.BarData;
+import com.github.mikephil.charting.data.BarDataSet;
+import com.github.mikephil.charting.data.BarEntry;
+import com.github.mikephil.charting.formatter.IndexAxisValueFormatter;
+import com.github.mikephil.charting.utils.ColorTemplate;
+
+
+
+
+public class MainActivity extends AppCompatActivity {
+
+    protected Interpreter tflite;
+    private MappedByteBuffer tfliteModel;
+    private TensorImage inputImageBuffer;
+    private  int imageSizeX;
+    private  int imageSizeY;
+    private  TensorBuffer outputProbabilityBuffer;
+    private  TensorProcessor probabilityProcessor;
+    private static final float IMAGE_MEAN = 0.0f;
+    private static final float IMAGE_STD = 1.0f;
+    private static final float PROBABILITY_MEAN = 0.0f;
+    private static final float PROBABILITY_STD = 255.0f;
+    private Bitmap bitmap;
+    private List<String> labels;
+    private HorizontalBarChart mBarChart;
+    ImageView imageView;
+    Uri imageuri;
+    Button buclassify;
+
+    TextView prediction;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        imageView=(ImageView)findViewById(R.id.image);
+        buclassify=(Button)findViewById(R.id.classify);
+        prediction=(TextView)findViewById(R.id.predictions);
+
+
+
+
+
+
+        imageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent();
+                intent.setType("image/*");
+                intent.setAction(Intent.ACTION_GET_CONTENT);
+                startActivityForResult(Intent.createChooser(intent,"Select Picture"),12);
+            }
+        });
+
+        try{
+            tflite=new Interpreter(loadmodelfile(MainActivity.this));
+        }catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        buclassify.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                int imageTensorIndex = 0;
+                int[] imageShape = tflite.getInputTensor(imageTensorIndex).shape(); // {1, height, width, 3}
+                imageSizeY = imageShape[1];
+                imageSizeX = imageShape[2];
+                DataType imageDataType = tflite.getInputTensor(imageTensorIndex).dataType();
+
+                int probabilityTensorIndex = 0;
+                int[] probabilityShape =
+                        tflite.getOutputTensor(probabilityTensorIndex).shape(); // {1, NUM_CLASSES}
+                DataType probabilityDataType = tflite.getOutputTensor(probabilityTensorIndex).dataType();
+
+                inputImageBuffer = new TensorImage(imageDataType);
+                outputProbabilityBuffer = TensorBuffer.createFixedSize(probabilityShape, probabilityDataType);
+                probabilityProcessor = new TensorProcessor.Builder().add(getPostprocessNormalizeOp()).build();
+
+                inputImageBuffer = loadImage(bitmap);
+
+                tflite.run(inputImageBuffer.getBuffer(),outputProbabilityBuffer.getBuffer().rewind());
+                showresult();
+            }
+        });
+
+
+
+    }
+
+
+
+
+
+
+
+
+    private TensorImage loadImage(final Bitmap bitmap) {
+        // Loads bitmap into a TensorImage.
+        inputImageBuffer.load(bitmap);
+
+        // Creates processor for the TensorImage.
+        int cropSize = Math.min(bitmap.getWidth(), bitmap.getHeight());
+        // TODO(b/143564309): Fuse ops inside ImageProcessor.
+        ImageProcessor imageProcessor =
+                new ImageProcessor.Builder()
+                        .add(new ResizeWithCropOrPadOp(cropSize, cropSize))
+                        .add(new ResizeOp(imageSizeX, imageSizeY, ResizeOp.ResizeMethod.NEAREST_NEIGHBOR))
+                        .add(getPreprocessNormalizeOp())
+                        .build();
+        return imageProcessor.process(inputImageBuffer);
+    }
+
+    private MappedByteBuffer loadmodelfile(Activity activity) throws IOException {
+        AssetFileDescriptor fileDescriptor=activity.getAssets().openFd("cartoon_model.tflite");
+        FileInputStream inputStream=new FileInputStream(fileDescriptor.getFileDescriptor());
+        FileChannel fileChannel=inputStream.getChannel();
+        long startoffset = fileDescriptor.getStartOffset();
+        long declaredLength=fileDescriptor.getDeclaredLength();
+        return fileChannel.map(FileChannel.MapMode.READ_ONLY,startoffset,declaredLength);
+    }
+
+    private TensorOperator getPreprocessNormalizeOp() {
+        return new NormalizeOp(IMAGE_MEAN, IMAGE_STD);
+    }
+    private TensorOperator getPostprocessNormalizeOp(){
+        return new NormalizeOp(PROBABILITY_MEAN, PROBABILITY_STD);
+    }
+
+    public static void barchart(BarChart barChart, ArrayList<BarEntry> arrayList, final ArrayList<String> xAxisValues) {
+        barChart.setDrawBarShadow(false);
+        barChart.setFitBars(true);
+        barChart.setDrawValueAboveBar(true);
+        barChart.setMaxVisibleValueCount(25);
+        barChart.setPinchZoom(true);
+
+        barChart.setDrawGridBackground(true);
+        BarDataSet barDataSet = new BarDataSet(arrayList, "Class");
+        barDataSet.setColors(new int[]{Color.parseColor("#03A9F4"), Color.parseColor("#FF9800"),
+        Color.parseColor("#76FF03"), Color.parseColor("#E91E63"), Color.parseColor("#2962FF")});
+        //barDataSet.setColors(ColorTemplate.COLORFUL_COLORS);
+        BarData barData = new BarData(barDataSet);
+        barData.setBarWidth(0.9f);
+        barData.setValueTextSize(0f);
+
+        barChart.setBackgroundColor(Color.TRANSPARENT); //set whatever color you prefer
+        barChart.setDrawGridBackground(false);
+        barChart.animateY(2000);
+
+        //Legend l = barChart.getLegend(); // Customize the ledgends
+        //l.setTextSize(10f);
+        //l.setFormSize(10f);
+//To set components of x axis
+        XAxis xAxis = barChart.getXAxis();
+        xAxis.setTextSize(13f);
+        xAxis.setPosition(XAxis.XAxisPosition.TOP_INSIDE);
+        xAxis.setValueFormatter(new IndexAxisValueFormatter(xAxisValues));
+        xAxis.setDrawGridLines(false);
+
+        barChart.setData(barData);
+
+    }
+
+
+    private void showresult(){
+
+        try{
+            labels = FileUtil.loadLabels(MainActivity.this,"cartoon_labels.txt");
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        Map<String, Float> labeledProbability =
+                new TensorLabel(labels, probabilityProcessor.process(outputProbabilityBuffer))
+                        .getMapWithFloatValue();
+        float maxValueInMap =(Collections.max(labeledProbability.values()));
+
+        for (Map.Entry<String, Float> entry : labeledProbability.entrySet()) {
+            //if (entry.getValue()==maxValueInMap) {
+            String[] label = labeledProbability.keySet().toArray(new String[0]);
+            Float[] label_probability = labeledProbability.values().toArray(new Float[0]);
+
+            mBarChart = findViewById(R.id.chart);
+            mBarChart.getXAxis().setDrawGridLines(false);
+            mBarChart.getAxisLeft().setDrawGridLines(false);
+            // PREPARING THE ARRAY LIST OF BAR ENTRIES
+            ArrayList<BarEntry> barEntries = new ArrayList<>();
+            for(int i=0; i<label_probability.length; i++)
+            {
+                barEntries.add(new BarEntry(i, label_probability[i]*100));
+            }
+
+        // TO ADD THE VALUES IN X-AXIS
+            ArrayList<String> xAxisName = new ArrayList<>();
+            for(int i=0;i<label.length; i++)
+            {
+                xAxisName.add(label[i]);
+            }
+            barchart(mBarChart,barEntries,xAxisName);
+            prediction.setText("Predictions:");
+
+
+ //           }
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(requestCode==12 && resultCode==RESULT_OK && data!=null) {
+            imageuri = data.getData();
+            try {
+                bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), imageuri);
+                imageView.setImageBitmap(bitmap);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
+
